51e8571fcef62033737030f4b55f9d8adfe11e3f
叶志晟 <yzs981130@126.com>
2016-06-11 09:34:09 +0800

    Codeforces 508B Anton and currency you all know

--- textcode/源.cpp
+++ textcode/源.cpp
@@ -1,19 +1,54 @@
-#include <cstdio>
-#include <cstring>
 #include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
 using namespace std;
-const int N = 10;
+bool is_all_odd(string n)
+{
+    for (int i = 0; i < n.length(); i++)
+        if ((n[i] - '0') % 2 == 0)
+            return false;
+    return true;
+}
 int main()
 {
-    int n, k, a, c = 0;
-    cin >> n >> k;
-    k = 5 - k;
-    for (int i = 0; i < n; i++)
+    string n;
+    cin >> n;
+    if (is_all_odd(n))
+        cout << "-1" << endl;
+    else
     {
-        cin >> a;
-        if (a <= k)
-            c++;
+        bool flag = false;
+        int pos = string::npos;
+        int len = n.size();
+        int n_l = n[len - 1] - '0';
+        for (int i = 0; i < n.length(); i++)
+        {
+            int t = n[i] - '0';
+            if (t % 2 == 0)
+            {
+                if (n_l > t)
+                {
+                    char tmp = n[len - 1];
+                    n[len - 1] = n[i];
+                    n[i] = tmp;
+                    flag = true;
+                    pos = i;
+                    break;
+                }
+                else
+                    pos = i;
+            }
+        }
+        cout << flag << ' ' << pos << endl;
+        if (!flag && pos != string::npos)
+        {
+            char tmp = n[len - 1];
+            n[len - 1] = n[pos];
+            n[pos] = tmp;
+        }
+        cout << n << endl;
     }
-    cout << c / 3 << endl;
+    system("pause");
     return 0;
 }
