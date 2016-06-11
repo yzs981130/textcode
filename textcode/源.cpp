93c2c59c9d0dcdd184b2937b403dfc79f494374c
叶志晟 <yzs981130@126.com>
2016-06-11 08:46:46 +0800

    http://codeforces.com/problemset/problem/432/A

--- textcode/源.cpp
+++ textcode/源.cpp
@@ -1,41 +1,19 @@
-#include <stdio.h> 
-#include <time.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#define PRIME_LIM 10000000 
-#define N 100000000 
-int primes[PRIME_LIM] = {0}; 
-int flags[N/96 + 1] = {0}; 
-int get_prime() 
-{ 
-    int nu = 5, to = 0; 
-    primes[0] = 2; 
-    primes[1] = 2, primes[2] = 3; 
-    for(int i = 0; nu <= N; i++) 
-    { 
-        if(!(flags[i>>5]&(1<<(i&31)))) 
-            primes[++primes[0]] = nu; 
-        for(int j = 3; j <= primes[0] && primes[j] * nu <= N; j++) 
-        { 
-            to = (nu * primes[j] - 5) >> 1; 
-            to -= to/3; 
-            flags[to>>5] |= 1<<(to&31); 
-            if(!(nu % primes[j])) 
-                break; 
-        } 
-        nu += 2 + ((i&1) << 1); 
-    } 
-    return primes[0]; 
-} 
-int main() 
-{ 
-    clock_t t = clock(); 
-    printf("%d\n", get_prime()); 
-    printf("Time:%f\n", 1.0 * (clock() - t) / CLOCKS_PER_SEC); 
-    for(int i = 1; primes[i] < 100; i++) 
-    { 
-        printf("%d\n", primes[i]); 
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+const int N = 10;
+int main()
+{
+    int n, k, a, c = 0;
+    cin >> n >> k;
+    k = 5 - k;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        if (a <= k)
+            c++;
     }
-    system("pause");
-    return 0; 
+    cout << c / 3 << endl;
+    return 0;
 }
