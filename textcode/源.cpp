87b85a4424649b4a26d21431bc0f486781610f1b
叶志晟 <yzs981130@126.com>
2016-06-11 10:56:35 +0800

    CF 500 new year book reading

--- textcode/源.cpp
+++ textcode/源.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <vector>
 using namespace std;
+int w[502], b[1002];
+bool vis[502];
 int main()
 {
-    int zero, one;
-    cin >> zero >> one;
-    if (one < zero - 1 || one > 2 * (zero + 1))
-        cout << "-1";
-    else if (one == zero || zero == one + 1)
+    int n, m, ans = 0;
+    vector<int> v;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        cin >> w[i];
+    for (int i = 1; i <= m; i++)
     {
-        for (int i = 0; i < zero - 1; i++)
-            cout << "01";
-        if (one == zero)
-            cout << "01";
-        else
-            cout << "0";
+        cin >> b[i];
+        if (!vis[b[i]])
+        {
+            v.push_back(b[i]);
+            vis[b[i]] = true;
+        }
     }
-    else if (zero < one && one <= zero * 2)
+    for (int i = 2; i <= m; i++)
     {
-        for (int i = 0; i < one - zero; i++)
-            cout << "110";
-        for (int i = 0; i < 2 * zero - one; i++)
-            cout << "10";
+        int j = 0;
+        while (v[j] != b[i])
+        {
+            ans += w[v[j]];
+            j++;
+        }
+        v.erase(v.begin() + j);
+        v.insert(v.begin(), b[i]);
     }
-    else if (zero * 2 < one && one <= 2 * (zero + 1))
-    {
-        for (int i = 0; i < zero; i++)
-            cout << "110";
-        for (int i = 0; i < one - 2 * zero; i++)
-            cout << "1";
-    }
-    cout << endl;
+    cout << ans << endl;
     system("pause");
     return 0;
 }
